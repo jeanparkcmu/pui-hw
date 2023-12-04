800fa6126a68c872466683eec7a2568a21681f59
parkjean03 <jeanp@andrew.cmu.edu>
2023-12-04 04:44:27 -0500

    pui-final-temp

---
+++ pui-final-project/model.js
@@ -0,0 +1,52 @@
+import * as THREE from "three";
+console.log(THREE);
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"; 
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.outputColorSpace = THREE.SRGBColorSpace; 
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio); 
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0,5,-3); 
+camera.lookAt(1, 1, 1); 
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; 
+controls.enablePan = false;
+controls.minDistance = 5; 
+controls.maxDistance = 5; 
+controls.minPolarAngle = 0.5; 
+controls.maxPolarAngle = 1.5; 
+controls.autoRotate = false; 
+controls.target = new THREE.Vector3(1, 1, 0); 
+controls.update(); 
+
+const spotLight = new THREE.SpotLight(0xffffff, 5, 100, 0.2, 0.5);
+spotLight.position.set(0, 25, 0); 
+scene.add(spotLight); 
+
+let object; 
+
+const loader = new GLTFLoader(); 
+
+loader.load('./poly.glb', function(gltf) {
+        object = gltf.scene;
+        object.position.set(1, 1.05, 1);
+        scene.add(object);
+});
+
+function animate() {
+    requestAnimationFrame(animate); 
+    controls.update();
+    renderer.render(scene, camera);
+};
+
+animate(); 
